45cba9bc6bf23026823df57a6ad62cc0fd99a5bf
Raghav Vaid <raghav@mac.mynetworksettings.com>
2025-04-17 20:19:52 -0400

    Initial commit: CPU + RAM usage monitor

---
+++ monitor.py
@@ -0,0 +1,21 @@
+import psutil
+import time
+
+def get_cpu_ram_usage():
+    cpu = psutil.cpu_percent(interval=1)
+    ram = psutil.virtual_memory().percent
+    return cpu, ram
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    while True:
+        cpu, ram = get_cpu_ram_usage()
+        print(f"CPU usage: {cpu}% | Ram Usage: {ram}%")
+        time.sleep(1)
+
+
